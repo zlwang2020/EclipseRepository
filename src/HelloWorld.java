8b805207f2aadd4bef618de4a8f1858cfdddb6f6
zlwang2020 <wzl2012@tju.edu.cn>
2020-05-05 20:28:48 +0800

    junit注解测试

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,6 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
@@ -5,5 +8,19 @@ public class HelloWorld {
 		System.out.println("Welcome to Github");
 	}
 
+	
+	//Junit注解, before/after只能点test run as junit不然报错
+    @Test
+    public void testJunit(){
+    	System.out.println("hello junit");
+    }
+    @Before
+    public void testBefore(){
+    	System.out.println("before");
+    }
+    @After
+    public void testAfter(){
+    	System.out.println("after");
+    }
 
 }
